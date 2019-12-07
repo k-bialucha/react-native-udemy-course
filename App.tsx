57f0c390577d5d86a464ba29291fc0ee6deac548
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-12-07 21:20:21 +0100

    L24: styles extracted to the StyleSheet object

--- App.tsx
+++ App.tsx
@@ -3,24 +3,9 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ padding: 30 }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          marginBottom: 15,
-        }}
-      >
-        <TextInput
-          placeholder="Course goal..."
-          style={{
-            borderColor: '#bbb',
-            borderWidth: 2,
-            padding: 5,
-            flexBasis: '70%',
-          }}
-        />
+    <View style={styles.container}>
+      <View style={styles.inputArea}>
+        <TextInput placeholder="Course goal..." style={styles.textInput} />
         <Button title="add" onPress={null} />
       </View>
       <View>
@@ -30,4 +15,18 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { padding: 30 },
+  inputArea: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 15,
+  },
+  textInput: {
+    borderColor: '#bbb',
+    borderWidth: 2,
+    padding: 5,
+    flexBasis: '70%',
+  },
+});
