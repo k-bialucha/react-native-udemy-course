73d8b0c5a592d68820c9d26e8fc44923e6f33e13
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-12-19 00:11:11 +0100

    L52: Header component created

---
+++ app-1-guess-a-number/components/Header.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+interface Props {
+  title: string;
+}
+
+const Header: React.FC<Props> = ({ title }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 80,
+    paddingTop: 30,
+    paddingBottom: 5,
+    paddingHorizontal: 10,
+
+    backgroundColor: 'dodgerblue',
+    justifyContent: 'flex-end',
+  },
+  title: {
+    color: 'white',
+    fontSize: 32,
+  },
+});
+
+export default Header;
