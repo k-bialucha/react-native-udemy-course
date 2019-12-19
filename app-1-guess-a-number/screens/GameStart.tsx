7837f48608757db6a583b04c1ee249e560ac1a0c
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-12-19 01:01:40 +0100

    L54: basic elements added to the GameStart component

--- app-1-guess-a-number/screens/GameStart.tsx
+++ app-1-guess-a-number/screens/GameStart.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 const GameStart: React.FC<{}> = () => {
   return (
     <View style={styles.screen}>
-      <Text>Game Start Screen</Text>
+      <Text>Start a New Game</Text>
+      <View style={styles.card}>
+        <Text style={styles.title}>Select a Number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -14,6 +22,23 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     padding: 10
+  },
+  title: {
+    fontSize: 30,
+    marginVertical: 10,
+    color: "dodgerblue",
+    fontWeight: "600"
+  },
+  card: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center"
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 30
   }
 });
 
