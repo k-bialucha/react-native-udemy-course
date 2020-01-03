1f534914fe9f7ae84184b863f53b2d5951cc62b6
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-01-03 03:06:28 +0100

    L64: basic guessing fetaure implemented; styles added

--- app-1-guess-a-number/screens/Game.tsx
+++ app-1-guess-a-number/screens/Game.tsx
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-interface Props {}
+import AppTheme from "../AppTheme";
 
-const GameScreen: React.FC<Props> = () => {
+interface Props {
+  userChoice: number;
+}
+
+const generateRandomNumber = (
+  min: number,
+  max: number,
+  excludedNumbers: number[]
+) => {
+  const minAdjusted: number = Math.ceil(min);
+  const maxAdjusted: number = Math.floor(max);
+
+  const amplitude: number = maxAdjusted - minAdjusted;
+
+  const generatedNumber: number =
+    Math.floor(Math.random() * amplitude) + minAdjusted;
+
+  if (excludedNumbers.includes(generatedNumber))
+    return generateRandomNumber(min, max, excludedNumbers);
+
+  return generatedNumber;
+};
+
+const GameScreen: React.FC<Props> = ({ userChoice }) => {
+  const [numberGuess, setNumberGuess] = useState(
+    generateRandomNumber(1, 100, [userChoice])
+  );
   return (
-    <View>
-      <Text>Game Screen</Text>
+    <View style={styles.container}>
+      <Text>Game Screen - my guess is</Text>
+      <View>
+        <Text style={styles.number}>{numberGuess}</Text>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 10
+  },
+  number: {
+    fontSize: 36,
+    color: AppTheme.primary
+  }
+});
 
 export default GameScreen;
