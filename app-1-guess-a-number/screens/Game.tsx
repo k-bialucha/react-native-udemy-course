afa4231f62af85f68d42f6c3a26d83c969577495
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-01-03 02:48:51 +0100

    L64: initial Game screen created

---
+++ app-1-guess-a-number/screens/Game.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+interface Props {}
+
+const GameScreen: React.FC<Props> = () => {
+  return (
+    <View>
+      <Text>Game Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default GameScreen;
