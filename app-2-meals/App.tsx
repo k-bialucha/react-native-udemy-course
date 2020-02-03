fc55bd1b838b3eb3eb48a5a2d9e3bf475eb189c0
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-02-03 02:19:05 +0100

    L108: font loading set up

--- app-2-meals/App.tsx
+++ app-2-meals/App.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+
+const loadFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
 
 export default function App() {
+  const [isAppLoaded, setIsAppLoaded] = useState<boolean>(false);
+
+  if (!isAppLoaded)
+    return (
+      <AppLoading
+        startAsync={loadFonts}
+        onFinish={() => setIsAppLoaded(false)}
+      />
+    );
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
