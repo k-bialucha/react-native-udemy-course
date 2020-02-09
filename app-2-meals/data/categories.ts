e9c9406c45f7e5c4fe60996af46aba0a7a176b92
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-02-10 00:50:15 +0100

    L116: Category class defined; categories list created

---
+++ app-2-meals/data/categories.ts
@@ -0,0 +1,16 @@
+import Category from '../models/Category';
+
+const categories: Category[] = [
+  new Category('c1', 'Italian', '#142850'),
+  new Category('c2', "Quick'n'easy", '#27496d'),
+  new Category('c3', 'Hamburgers', '#0c7b93'),
+  new Category('c4', 'German', '#00a8cc'),
+  new Category('c5', "Light'n'lovely", '#54123b'),
+  new Category('c6', 'Exotic', '#84142d'),
+  new Category('c7', 'Breakfast', '#c02739'),
+  new Category('c8', 'Asian', '#29c7ac'),
+  new Category('c9', 'French', '#5b5656'),
+  new Category('c10', 'Summer', '#4d4646'),
+];
+
+export default categories;
