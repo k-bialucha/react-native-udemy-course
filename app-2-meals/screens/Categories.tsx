8f5cfa84f7233b0bc7fe4764c04515ac886b28e5
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-02-10 01:21:35 +0100

    L116: FlatList implemented in the Categories screen

--- app-2-meals/screens/Categories.tsx
+++ app-2-meals/screens/Categories.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { NavigationStackProp } from 'react-navigation-stack';
 
-import { CATEGORY_MEALS_SCREEN_NAME } from './CategoryMeals';
+import CategoryItem from '../components/CategoryItem';
+import categories from '../data/categories';
+
+// import { CATEGORY_MEALS_SCREEN_NAME } from './CategoryMeals';
 
 interface Props {
   navigation: NavigationStackProp<{}>;
@@ -12,16 +15,38 @@ export const CATEGORIES_SCREEN_NAME = 'categories';
 
 const CategoriesScreen: React.FC<Props> = props => {
   return (
-    <View>
+    <View style={styles.screen}>
       <Text>Categories</Text>
-      <Button
-        title="Go to Meals"
-        onPress={() => {
-          props.navigation.navigate(CATEGORY_MEALS_SCREEN_NAME);
-        }}
+      <FlatList
+        data={categories}
+        renderItem={item => (
+          <CategoryItem
+            title={item.item.title}
+            style={{ ...styles.listItem, backgroundColor: item.item.color }}
+          />
+        )}
+        numColumns={2}
+        style={styles.list}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  list: {
+    flex: 1,
+    width: '100%',
+  },
+  listItem: {
+    flex: 1,
+    height: 150,
+    margin: 15,
+  },
+});
 
 export default CategoriesScreen;
