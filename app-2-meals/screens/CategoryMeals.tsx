afd273dd5ca6a7c5e8a84d3f2c5241782ffe5d7a
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-02-11 00:01:27 +0100

    L119: dynamic navigationOptions added in the CategoryMeals screen

--- app-2-meals/screens/CategoryMeals.tsx
+++ app-2-meals/screens/CategoryMeals.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
-import { NavigationStackProp } from 'react-navigation-stack';
+import { Button, Text, View, Platform } from 'react-native';
+import { NavigationStackScreenComponent } from 'react-navigation-stack';
 
 import Category from '../models/Category';
 import categories from '../data/categories';
 
 import { MEAL_DETAILS_SCREEN_NAME } from './MealDetail';
 
-interface Props {
-  navigation: NavigationStackProp<{}>;
-}
-
 export const CATEGORY_MEALS_SCREEN_NAME = 'categoryMeals';
 
-const CategoryMealsScreen: React.FC<Props> = ({ navigation }) => {
+const CategoryMealsScreen: NavigationStackScreenComponent = ({
+  navigation,
+}) => {
   const categoryId = navigation.getParam('categoryId');
 
   const category: Category = categories.find(
@@ -32,6 +30,22 @@ const CategoryMealsScreen: React.FC<Props> = ({ navigation }) => {
       />
     </View>
   );
+};
+
+CategoryMealsScreen.navigationOptions = ({ navigation }) => {
+  const categoryId = navigation.getParam('categoryId');
+
+  const { title, color } = categories.find(
+    category => category.id === categoryId
+  );
+
+  return {
+    title: title.toUpperCase(),
+    headerStyle: {
+      backgroundColor: Platform.OS === 'android' ? color : null,
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : color,
+  };
 };
 
 export default CategoryMealsScreen;
