c5cfc181dccb227c70e6ab212edb72c61497c2bf
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-02-12 01:38:15 +0100

    L121: CategoryItem stylng adjusted

--- app-2-meals/components/CategoryItem.tsx
+++ app-2-meals/components/CategoryItem.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const CategoryItem: React.FC<Props> = ({ title, style = {}, onPress }) => {
   return (
-    <View style={style}>
+    <View style={{ ...styles.main, ...style }}>
       <TouchableOpacity onPress={onPress}>
         <View style={styles.tile}>
           <Text style={styles.title}>{title}</Text>
@@ -21,16 +21,27 @@ const CategoryItem: React.FC<Props> = ({ title, style = {}, onPress }) => {
 };
 
 const styles = StyleSheet.create({
+  main: {
+    borderRadius: 16,
+  },
   tile: {
     width: '100%',
     height: '100%',
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 10,
+    elevation: 3,
+    padding: 15,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
   },
   title: {
     color: 'white',
-    fontSize: 24,
+    fontSize: 22,
     fontWeight: '600',
-    textAlign: 'center',
-    paddingVertical: 30,
+    textAlign: 'right',
+    fontFamily: 'poppins',
   },
 });
 
